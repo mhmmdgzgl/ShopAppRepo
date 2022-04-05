e94fc4fa6f57fcdd34d25dbcc5781e30674a8f03
muhammed gezgel <gezgelmuhammed@outlook.com>
2022-04-05 16:47:36 +0300

    a few changes..

---
+++ ShopApp.WebUI/Controllers/ShopController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShopApp.WebUI.Controllers;
+
+public class ShopController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
